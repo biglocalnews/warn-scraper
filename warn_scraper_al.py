b95a71adfff14609ffbfe0b4bcc4fe4b130c620e
Dilcia19 <dilcia19@stanford.edu>
2020-06-02 10:52:54 -0700

    update al action

--- warn_scraper_al.py
+++ warn_scraper_al.py
@@ -1,4 +1,5 @@
 from os import path
+import pathlib
 
 import csv 
 from datetime import datetime
@@ -10,8 +11,11 @@ import json
 # spot-checked and linked-checked
 # scraper looks good
 
+root = pathlib.Path(__file__).parent.resolve()
+
 def alabama():
-    output_csv = '/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/alabama_warn_raw.csv'
+    output_csv = root / 'data' / 'alabama_warn_raw.csv'
+    #output_csv = '/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/alabama_warn_raw.csv'
     url = 'https://www.madeinalabama.com/warn-list/'
     page = requests.get(url)
 
