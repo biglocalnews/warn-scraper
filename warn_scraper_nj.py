6c738365ae1904defc16c2ebfeec9899e4f36043
Dilcia19 <dilcia19@stanford.edu>
2020-06-02 11:09:08 -0700

    update nj action

--- warn_scraper_nj.py
+++ warn_scraper_nj.py
@@ -1,4 +1,5 @@
 from os import path
+import pathlib
 
 import csv 
 from datetime import datetime
@@ -10,8 +11,13 @@ import json
 # spot-checked and linked-checked
 # scraper looks good
 
+root = pathlib.Path(__file__).parent.resolve()
+
+
 def newjersey():
-    output_csv = '/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/newjersey_warn_raw.csv'
+    output_csv = root / 'data' / 'newjersey_warn_raw.csv'
+
+    #output_csv = '/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/newjersey_warn_raw.csv'
 
     url_main = 'http://lwd.state.nj.us/WorkForceDirectory/warn.jsp'
 
