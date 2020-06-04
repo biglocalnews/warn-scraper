a2398533aab3745cb2e51b292237f1223c84bc88
Dilcia19 <dilcia19@stanford.edu>
2020-06-04 11:54:23 -0700

    udpate ne action

--- combine_ne_scrapers.py
+++ combine_ne_scrapers.py
@@ -1,5 +1,6 @@
 from os import path
 import os
+import pathlib
 
 import csv 
 from datetime import datetime
@@ -9,10 +10,13 @@ import requests
 import json
 import pandas as pd
 
-def combine():
+root = pathlib.Path(__file__).parent.resolve()
 
-    ne_one = pd.read_csv('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw1.csv')
-    ne_two = pd.read_csv('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw2.csv')
+def combine():
+    ne_one = root / 'data' / 'nebraska_warn_raw1.csv'
+    ne_two = root / 'data' / 'nebraska_warn_raw2.csv'
+    #ne_one = pd.read_csv('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw1.csv')
+    #ne_two = pd.read_csv('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw2.csv')
     ne_all_data = pd.concat([ne_one, ne_two])
     ne_all_data.to_csv('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw.csv')
     os.remove('/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/nebraska_warn_raw1.csv')
