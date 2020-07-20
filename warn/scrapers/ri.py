b5ca33116ded2583694b173fc966909fcf3fd5f7
Dilcia Mercedes <dilcia19@stanford.edu>
2020-07-20 16:26:47 -0700

    updated rhode island script

--- warn/scrapers/ri.py
+++ warn/scrapers/ri.py
@@ -5,52 +5,44 @@ from bs4 import BeautifulSoup
 
 # spot-check once more
 
-'''
-Current url is no longer working. Have to find new one and update the scraper, if needed. (7/16/20)
-'''
-
 def scrape(output_dir):
 
     # output_csv = '/Users/dilcia_mercedes/Big_Local_News/prog/WARN/data/rhodeisland_warn_raw.csv'
     output_csv = '{}/rhodeisland_warn_raw.csv'.format(output_dir)
-    url = 'http://www.dlt.ri.gov/bwc/WARN.htm'
+    url = 'https://dlt.ri.gov/wds/warn/'
     page = requests.get(url)
-
-    page.status_code # should be 200
-
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    table = soup.find_all('table') # output is list-type
-    len(table)
+    tables = soup.find_all('table') # output is list-type
+    print(len(tables))
 
-    # find header
-    first_row = table[6].find_all('tr')[1]
-    headers = first_row.find_all('td')
+    first_row = tables[0].find_all('tr')[0]
+    headers = first_row.find_all('th')
     output_header = []
     for header in headers:
         output_header.append(header.text)
-    # output_header = [x.strip().replace("\r\n","").replace("\s", "") for x in output_header]
-    output_header = [' '.join(x.split()) for x in output_header]
-    output_header
-
-    # if len(table) == 1:
-    output_rows = []
-    for table_row in table[6].find_all('tr'):    
-        columns = table_row.find_all('td')
-        output_row = []
-        for column in columns:
-            output_row.append(column.text)
-        output_row = [x.strip() for x in output_row]
-        output_rows.append(output_row)
-    # remove first empty row
-    output_rows.pop(0)
-    output_rows.pop(0)
-    output_rows.pop(0)
+    output_header = [x.strip() for x in output_header]
+    print(output_header)
 
     with open(output_csv, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
-        writer.writerows(output_rows)
+
+    for table in tables:
+        output_rows = []
+        for table_row in table.find_all('tr'):    
+            columns = table_row.find_all('td')
+            output_row = []
+            for column in columns:
+                output_row.append(column.text)
+            output_row = [x.strip() for x in output_row]
+            output_rows.append(output_row)
+        output_rows.pop(0)
+        
+        if len(output_rows) > 0:
+            with open(output_csv, 'a') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(output_rows)   
 
 if __name__ == '__main__':
     scrape()
