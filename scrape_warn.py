cbb042ca7a9680ac8ae0bbbfc1c284611b2233c2
Dilcia Mercedes <dilcia19@stanford.edu>
2020-08-17 13:38:35 -0700

    failing to implement slack alerts

--- scrape_warn.py
+++ scrape_warn.py
@@ -85,6 +85,7 @@ def scrape_warn_site(state, output_dir, cache_dir, alert):
     #alert_manager=alert_manager
     #slackAlertManager(alert_manager=alert_manager)
     #not sure how to initiate slack alert manager
+    #slack_class = SlackAlertManager() # needs API key and channel name
 
 
     state_clean = state.strip().lower()
@@ -97,7 +98,8 @@ def scrape_warn_site(state, output_dir, cache_dir, alert):
             #alert.send() #is a bool, doesn't work
             #alerts.send() #alerts is not defined, doesn't work
             print("Right before .send() ")
-            alert_manager.send()
+            #alert_manager.send()
+            #slack_class.send()
             print("We don't know what to add here.")
     except Exception as e:
         traceback_str = ''.join(traceback.format_tb(e.__traceback__))
