26cd6b0deed237bce9f3d10d7c2aa314341e819c
Dilcia Mercedes <dilcia19@stanford.edu>
2020-10-27 19:55:23 -0700

    added scraper for alaska

--- warn/scrapers/ak.py
+++ warn/scrapers/ak.py
@@ -10,30 +10,15 @@ def scrape(output_dir):
 
     logger = logging.getLogger(__name__)
     output_csv = '{}/alaska_warn_raw.csv'.format(output_dir)
-    # output_csv = '{}/alaska_warn_raw.csv'.format(new_dir)
 
-    # url = 'https://jobs.alaska.gov/RR/WARN_notices.htm'
-    url = 'https://jobs.alaska.gov/RR/WARN_notices.htmz'
+    url = 'https://jobs.alaska.gov/RR/WARN_notices.htm'
 
     page = requests.get(url)
     logger.info("Page status code is {}".format(page.status_code))
 
     soup = BeautifulSoup(page.text, 'html.parser')
-    # print(soup)
 
     table = soup.find_all('table') # output is list-type
-    output_header = [
-        'Company',
-        'Location',
-        'Notice Date', 
-        'Layoff Date', 
-        'Employees Affected', 
-        'Notes'
-        ]
-
-    with open(output_csv, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(output_header)
 
     output_rows = []
     for table_row in table[0].find_all('tr'):    
@@ -44,10 +29,15 @@ def scrape(output_dir):
         output_row = [x.strip() for x in output_row]
         output_rows.append(output_row)
 
-    # print(output_rows)
-    
+    for row in output_rows:
+        if row == ['']:
+            output_rows.remove(row)
 
-    
+
+    with open(output_csv, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(output_rows)
+
 
 if __name__ == '__main__':
     scrape()
