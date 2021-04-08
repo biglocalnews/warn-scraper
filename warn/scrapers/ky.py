d5c18e2fc5d6ba5034b37dfba0580e4f10d86896
Dilcia Mercedes <dilcia19@stanford.edu>
2021-04-08 10:59:56 -0700

    added code for ky, it is not working all the way, need to troubleshoot

--- warn/scrapers/ky.py
+++ warn/scrapers/ky.py
@@ -1,20 +1,30 @@
 import os
 import logging
 import requests
+import xlrd
+import openpyxl
 import pandas as pd
 
 def scrape(output_dir):
 
-    # make sure this URL works, test on other days
     logger  = logging.getLogger(__name__)
-
     url = 'https://kcc.ky.gov/WARN%20notices/WARN%20Notices%202021/WARN%20Notice%20Report%2003222021%20.xlsx'
 
-    # several sheets in one excel file, will come back to
+    df = pd.read_excel(url, sheet_name = None)
+    empty_df = pd.DataFrame()
 
-    # df = pd.read_excel(url)
-    # output_file = '{}/iowa_warn_raw.csv'.format(output_dir)
-    # df.to_csv(output_file, index=False)
+    for sheet in df:
+        indiv_sheet = pd.read_excel(url, sheet_name = sheet)
+        indiv_sheet['Year'] = sheet
+        
+        if 'Unnamed: 11' in indiv_sheet.columns:
+            indiv_sheet.drop(columns='Unnamed: 11', inplace=True)
+        if 'County: County Name' in indiv_sheet.columns:
+            indiv_sheet.rename(columns={'County: County Name': 'County'}, inplace=True)
+        empty_df = pd.concat([empty_df, indiv_sheet])
+
+    output_file = '{}/kentucky_warn_raw.csv'.format(output_dir)
+    empty_df.to_csv(output_file, index=False)
 
 
 if __name__ == '__main__':
