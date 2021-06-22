8abeaf1352ec3b4d4684547ff06be1ff9838302b
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-22 11:27:40 -0700

    Add setup.py to allow pip install

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+requirements = [
+    'bs4',
+    'pandas',
+    'requests',
+    'xlrd',
+    # Install non-PyPI libraries
+    'bln-etl @ git+https://github.com/biglocalnews/bln-etl.git',
+]
+
+test_requirements = []
+
+setup(
+    name='warn',
+    version='0.1.0',
+    description="WARN Layoff notice scrapers.",
+    long_description="Web scrapers to gather WARN layoff data from state sites and upload to BLN platform.",
+    author="Dilcia Mercedes",
+    author_email='dilcia19@stanford.edu',
+    url='https://github.com/biglocalnews/WARN',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache 2.0 license",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
