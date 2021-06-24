d96f72c7e6b588270c9534c2381660d932f48cc8
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-23 19:16:30 -0700

    Standardize per #111

--- warn/scrapers/al.py
+++ warn/scrapers/al.py
@@ -1,29 +1,18 @@
 import csv
-import logging
-import requests 
+import requests
 
 from bs4 import BeautifulSoup
 
-# spot-check once more
 
 def scrape(output_dir):
-
-    logger = logging.getLogger(__name__)
     output_csv = '{}/alabama_warn_raw.csv'.format(output_dir)
     url = 'https://www.madeinalabama.com/warn-list/'
     page = requests.get(url)
-
     # can't see 2020 listings when I open web page, but they are on the summary in the google search
-
-    logger.info("Page status code is {}".format(page.status_code))
-
     soup = BeautifulSoup(page.text, 'html.parser')
-
     table = soup.find_all('table') # output is list-type
-
-    # if len(table) == 1:
     output_rows = []
-    for table_row in table[0].find_all('tr'):    
+    for table_row in table[0].find_all('tr'):
         columns = table_row.find_all('td')
         output_row = []
         for column in columns:
@@ -32,7 +21,6 @@ def scrape(output_dir):
         output_rows.append(output_row)
     # remove first empty row
     output_rows.pop(0)
-
     # find header
     first_row = table[0].find_all('tr')[0]
     headers = first_row.find_all('th')
@@ -41,14 +29,9 @@ def scrape(output_dir):
         output_header.append(header.text)
     output_header = [x.strip() for x in output_header]
     output_header
-
     with open(output_csv, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
         writer.writerows(output_rows)
-
-
-if __name__ == '__main__':
-    scrape()
-
+    return output_csv
 
