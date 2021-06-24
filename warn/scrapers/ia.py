401114f02ee7d45e9b5942ca8abbbf78cfd6c0e2
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-24 14:48:08 -0700

    Update IA per #111

--- warn/scrapers/ia.py
+++ warn/scrapers/ia.py
@@ -1,19 +1,14 @@
-import os
 import logging
-import requests
 import pandas as pd
 
-def scrape(output_dir):
+logger  = logging.getLogger(__name__)
 
+
+def scrape(output_dir, cache_dir=None):
     # make sure this URL works, test on other days
-    logger  = logging.getLogger(__name__)
-
     url = 'https://www.iowaworkforcedevelopment.gov/sites/search.iowaworkforcedevelopment.gov/files/documents/2018/WARN_20210331.xlsx'
-
     df = pd.read_excel(url)
     output_file = '{}/iowa_warn_raw.csv'.format(output_dir)
     df.to_csv(output_file, index=False)
+    return output_file
 
-
-if __name__ == '__main__':
-    scrape()
