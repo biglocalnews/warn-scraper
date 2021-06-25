5a4545b47da9dde489a3762fb696223bdc18a6a4
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-24 17:29:16 -0700

    Update MT per #111

--- warn/scrapers/mt.py
+++ warn/scrapers/mt.py
@@ -1,16 +1,9 @@
-import os
-import logging
-import requests
 import pandas as pd
 
-def scrape(output_dir):
 
-    logger  = logging.getLogger(__name__)
-
+def scrape(output_dir, cache_dir=None):
     url = 'https://wsd.dli.mt.gov/_docs/wioa/warn.xlsx'
     df = pd.read_excel(url)
     output_file = '{}/montana_warn_raw.csv'.format(output_dir)
     df.to_csv(output_file, index=False)
-
-if __name__ == '__main__':
-    scrape()
+    return output_file
