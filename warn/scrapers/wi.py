7f958b1818c2bb0c41611b7739830227dca748b8
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-25 08:45:27 -0700

    Update WI per #111

--- warn/scrapers/wi.py
+++ warn/scrapers/wi.py
@@ -5,45 +5,38 @@ import requests
 
 from bs4 import BeautifulSoup
 
-# spot-check once more
 
-def scrape(output_dir):
+logger = logging.getLogger(__name__)
 
-    logger = logging.getLogger(__name__)
-    output_csv = '{}/wisconsin_warn_raw.csv'.format(output_dir)
+
+def scrape(output_dir, cache_dir=None):
+    output_csv = f'{output_dir}/wisconsin_warn_raw.csv'
     years = [2016, 2017, 2018, 2019]
-
     url = 'https://sheets.googleapis.com/v4/spreadsheets/1cyZiHZcepBI7ShB3dMcRprUFRG24lbwEnEDRBMhAqsA/values/Originals?key=AIzaSyDP0OltIjcmRQ6-9TTmEVDZPIX6BSFcunw'
     response = requests.get(url)
-    logger.info(response.status_code)
+    logger.debug(f"Page status is {response.status_code} for {url}")
     data = response.json()
-
     # find header
     headers = data['values'][0]
     output_header = headers[3:len(headers)-1]
-
     output_rows = []
     for row in data['values'][1:len(data['values'])]:
         output_row = row[3:len(row)]
         output_rows.append(output_row)
-
     # save header
     with open(output_csv, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
         writer.writerows(output_rows)
-
     for year in years:
         url = 'https://dwd.wisconsin.gov/dislocatedworker/warn/{}/default.htm'.format(year)
         page = requests.get(url)
-
-        logger.info("Page status code is {}".format(page.status_code))
+        logger.debug(f"Page status is {page.status_code} for {url}")
         soup = BeautifulSoup(page.text, 'html.parser')
         tables = soup.find_all('table') # output is list-type
-        
         for table in tables:
             output_rows = []
-            for table_row in table.find_all('tr'):    
+            for table_row in table.find_all('tr'):
                 columns = table_row.find_all('td')
                 output_row = []
                 for column in columns:
@@ -54,17 +47,9 @@ def scrape(output_dir):
                     output_row.append(entry)
                 output_row = [x.strip() for x in output_row]
                 # filter "Updates to Previously Filed Notices"
-                if len(output_row) != 9:
-                    logger.info(output_row)
-                else:
-                    output_rows.append(output_row)
-                    
+                output_rows.append(output_row)
             if len(output_rows) > 0:
                 with open(output_csv, 'a') as csvfile:
                     writer = csv.writer(csvfile)
                     writer.writerows(output_rows)
-
-
-
-if __name__ == '__main__':
-    scrape()
+    return output_csv
