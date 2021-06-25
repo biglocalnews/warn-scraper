a7fb9b0ba7805c5d1ab7602f176865041faaa44d
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-25 08:41:00 -0700

    Update WA per #111

--- warn/scrapers/wa.py
+++ warn/scrapers/wa.py
@@ -4,26 +4,22 @@ import requests
 
 from bs4 import BeautifulSoup
 
-# spot-check once more
 
-def scrape(output_dir):
-    
-    logger = logging.getLogger(__name__)
-    output_csv = '{}/washington_warn_raw.csv'.format(output_dir)
+logger = logging.getLogger(__name__)
+
+
+def scrape(output_dir, cache_dir=None):
+    output_csv = f'{output_dir}/washington_warn_raw.csv'
     # pages = range(2, 60, 1)
     page = 2
-
     url = 'https://fortress.wa.gov/esd/file/warn/Public/SearchWARN.aspx'
-
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:68.0) Gecko/20100101 Firefox/68.0"}
     with requests.Session() as session:
-        # initial get
         response_initial = session.get(url, headers = headers) #, data = formdata)
-        logger.info(response_initial.status_code)
+        logger.debug(f"Page status is {response_initial.status_code} for {url}")
         soup_content = BeautifulSoup(response_initial.content, 'html5lib')
         soup_text = BeautifulSoup(response_initial.text, 'html5lib')
         table = soup_text.find_all('table') # output is list-type
-        
         # find and write header
         first_row = table[0].find_all('tr')[2]
         table_headers = first_row.find_all('th')
@@ -31,14 +27,12 @@ def scrape(output_dir):
         for table_header in table_headers:
             output_header.append(table_header.text)
         output_header = [x.strip() for x in output_header]
-        
         with open(output_csv, 'w') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(output_header)
-        
         # find and write first page
         output_rows = []
-        for table_row in table[0].find_all('tr'):    
+        for table_row in table[0].find_all('tr'):
             columns = table_row.find_all('td')
             output_row = []
             for column in columns:
@@ -46,30 +40,24 @@ def scrape(output_dir):
             output_row = [x.strip() for x in output_row]
             output_rows.append(output_row)
         output_rows = output_rows[3:len(output_rows)-2]
-        
         with open(output_csv, 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(output_rows)
-        
         while True:
             try:
-
-
                 formdata = {
                     '__EVENTTARGET': 'ucPSW$gvMain',
                     '__EVENTARGUMENT': 'Page${}'.format(page),
                     '__VIEWSTATE': soup_content.find('input', attrs={'name': '__VIEWSTATE'})['value'],
                     '__EVENTVALIDATION': soup_content.find('input', attrs={'name': '__EVENTVALIDATION'})['value']
                 }
-            
                 response_next = session.post(url, data = formdata)
-                logger.info(response_next.status_code) # should be 200
+                logger.debug(f"Page status is {response_next.status_code} for {url}")
                 soup_content = BeautifulSoup(response_next.content, 'html5lib')
                 soup_text = BeautifulSoup(response_next.text, 'html5lib')
                 table_next = soup_text.find_all('table') # output is list-type
-
                 output_rows = []
-                for table_row in table_next[0].find_all('tr'):    
+                for table_row in table_next[0].find_all('tr'):
                     columns = table_row.find_all('td')
                     output_row = []
                     for column in columns:
@@ -78,17 +66,10 @@ def scrape(output_dir):
                     output_rows.append(output_row)
                 output_rows = output_rows[3:len(output_rows)-2]
                 # first row of page
-                
                 with open(output_csv, 'a') as csvfile:
                     writer = csv.writer(csvfile)
                     writer.writerows(output_rows)
                 page += 1
-
             except:
                 break
-            
-
-
-
-if __name__ == '__main__':
-    scrape()
+    return output_csv
