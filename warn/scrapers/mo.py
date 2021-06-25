9d712a6757a3e2a76af9c6804468e0635af6a064
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-24 17:15:41 -0700

    Update MO per #111

--- warn/scrapers/mo.py
+++ warn/scrapers/mo.py
@@ -4,21 +4,17 @@ import requests
 
 from bs4 import BeautifulSoup
 
-# spot-check once more
+logger = logging.getLogger(__name__)
 
-def scrape(output_dir):
 
-    logger = logging.getLogger(__name__)
-    output_csv = '{}/missouri_warn_raw.csv'.format(output_dir)
+def scrape(output_dir, cache_dir=None):
+    output_csv = f'{output_dir}/missouri_warn_raw.csv'
     years = range(2018, 2014, -1)
-
     url = 'https://jobs.mo.gov/warn2019'
     page = requests.get(url)
-
-    logger.info("Page status code is {}".format(page.status_code))
+    logger.debug(f"Page status is {page.status_code} for {url}")
     soup = BeautifulSoup(page.text, 'html.parser')
     table = soup.find_all('table') # output is list-type
-
     # find header
     first_row = table[0].find_all('tr')[0]
     headers = first_row.find_all('th')
@@ -26,16 +22,13 @@ def scrape(output_dir):
     for header in headers:
         output_header.append(header.text)
     output_header = [x.strip() for x in output_header]
-
     # save header
     with open(output_csv, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
-
-
     # save 2019
     output_rows = []
-    for table_row in table[0].find_all('tr'):    
+    for table_row in table[0].find_all('tr'):
         columns = table_row.find_all('td')
         output_row = []
         for column in columns:
@@ -44,23 +37,19 @@ def scrape(output_dir):
         output_rows.append(output_row)
     output_rows.pop(len(output_rows) - 1) # pop "Total" row
     output_rows.pop(0) # pop header
-
     if len(output_rows) > 0:
         with open(output_csv, 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(output_rows)
-
-
     # save 2018-2015
     for year in years:
         url = 'https://jobs.mo.gov/warn{}'.format(year)
         page = requests.get(url)
-
+        logger.debug(f"Page status is {page.status_code} for {url}")
         soup = BeautifulSoup(page.text, 'html.parser')
         table = soup.find_all('table') # output is list-type
-        
         output_rows = []
-        for table_row in table[0].find_all('tr'):    
+        for table_row in table[0].find_all('tr'):
             columns = table_row.find_all('td')
             output_row = []
             for column in columns:
@@ -69,13 +58,8 @@ def scrape(output_dir):
             output_rows.append(output_row)
         output_rows.pop(len(output_rows)-1)
         output_rows.pop(0)
-
         if len(output_rows) > 0:
             with open(output_csv, 'a') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerows(output_rows)
-
-
-
-if __name__ == '__main__':
-    scrape()
+    return output_csv
