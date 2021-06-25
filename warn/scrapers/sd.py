1c606a09f00432ee9821414b007f5e1f04ad86c1
Serdar Tumgoren <zstumgoren@gmail.com>
2021-06-25 07:57:38 -0700

    Update SD per #111

--- warn/scrapers/sd.py
+++ warn/scrapers/sd.py
@@ -4,19 +4,17 @@ import requests
 
 from bs4 import BeautifulSoup
 
-# spot-check once more
 
-def scrape(output_dir):
+logger = logging.getLogger(__name__)
 
-    logger = logging.getLogger(__name__)
-    output_csv = '{}/southdakota_warn_raw.csv'.format(output_dir)
+
+def scrape(output_dir, cache_dir=None):
+    output_csv = f'{output_dir}/southdakota_warn_raw.csv'
     url = 'https://dlr.sd.gov/workforce_services/businesses/warn_notices.aspx'
     page = requests.get(url)
-
-    logger.info("Page status code is {}".format(page.status_code))
+    logger.debug(f"Page status is {page.status_code} for {url}")
     soup = BeautifulSoup(page.text, 'html.parser')
     table = soup.find_all('table') # output is list-type
-
     # find header
     first_row = table[0].find_all('tr')[0]
     headers = first_row.find_all('td')
@@ -25,10 +23,9 @@ def scrape(output_dir):
         output_header.append(header.text)
     # output_header = [x.strip().replace("\r\n","").replace("\s", "") for x in output_header]
     output_header = [' '.join(x.split()) for x in output_header]
-
     # if len(table) == 1:
     output_rows = []
-    for table_row in table[0].find_all('tr'):    
+    for table_row in table[0].find_all('tr'):
         columns = table_row.find_all('td')
         output_row = []
         for column in columns:
@@ -37,12 +34,8 @@ def scrape(output_dir):
         output_rows.append(output_row)
     # remove first empty row
     output_rows.pop(0)
-
     with open(output_csv, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
         writer.writerows(output_rows)
-
-
-if __name__ == '__main__':
-    scrape()
+    return output_csv
