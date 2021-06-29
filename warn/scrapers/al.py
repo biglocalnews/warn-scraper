6a6e751f2a2d34e68ae53085535117be7c816439
Cody Hergenroeder <chergenroeder135@gmail.com>
2021-06-29 16:58:10 -0700

    dropped dirty rows. added compatibility for windows display bugfix.

--- warn/scrapers/al.py
+++ warn/scrapers/al.py
@@ -1,8 +1,10 @@
 import csv
+from datetime import date
 import requests
 
 from bs4 import BeautifulSoup
 
+from warn.utils import write_rows_to_csv
 
 def scrape(output_dir, cache_dir=None):
     output_csv = '{}/alabama_warn_raw.csv'.format(output_dir)
@@ -21,6 +23,7 @@ def scrape(output_dir, cache_dir=None):
         output_rows.append(output_row)
     # remove first empty row
     output_rows.pop(0)
+
     # find header
     first_row = table[0].find_all('tr')[0]
     headers = first_row.find_all('th')
@@ -28,10 +31,12 @@ def scrape(output_dir, cache_dir=None):
     for header in headers:
         output_header.append(header.text)
     output_header = [x.strip() for x in output_header]
-    output_header
-    with open(output_csv, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(output_header)
-        writer.writerows(output_rows)
+    # add header to the top of the output file
+    output_rows.insert(0,output_header)
+
+    #remove the last 8 rows of dirty data
+    for x in range(8): output_rows.pop()
+
+    write_rows_to_csv(output_rows, output_csv)
     return output_csv
 
