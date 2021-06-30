552258bcb606cc52b1d979b8e2fbaca58246986b
Cody Hergenroeder <chergenroeder135@gmail.com>
2021-06-30 16:03:51 -0700

    alabama DRY extraction code

--- warn/scrapers/al.py
+++ warn/scrapers/al.py
@@ -1,5 +1,4 @@
 import csv
-from datetime import date
 import requests
 
 from bs4 import BeautifulSoup
@@ -13,30 +12,32 @@ def scrape(output_dir, cache_dir=None):
     # can't see 2020 listings when I open web page, but they are on the summary in the google search
     soup = BeautifulSoup(page.text, 'html.parser')
     table = soup.find_all('table') # output is list-type
+
     output_rows = []
     for table_row in table[0].find_all('tr'):
-        columns = table_row.find_all('td')
-        output_row = []
-        for column in columns:
-            output_row.append(column.text)
-        output_row = [x.strip() for x in output_row]
-        output_rows.append(output_row)
+        output_rows.append(extract_fields_from_row(table_row, 'td'))
+
     # remove first empty row
     output_rows.pop(0)
-
     # find header
     first_row = table[0].find_all('tr')[0]
-    headers = first_row.find_all('th')
-    output_header = []
-    for header in headers:
-        output_header.append(header.text)
-    output_header = [x.strip() for x in output_header]
     # add header to the top of the output file
-    output_rows.insert(0,output_header)
+    output_header = extract_fields_from_row(first_row, 'th')
+    output_rows.insert(0, output_header)
+    
 
     #remove the last 8 rows of dirty data
-    for x in range(8): output_rows.pop()
+    #TODO: replace with more robust regex strategy
+    output_rows = output_rows[0:-8]
 
     write_rows_to_csv(output_rows, output_csv)
+
     return output_csv
 
+#row is a beautifulsoup row object
+#element is the HTME element of the desired field
+def extract_fields_from_row(row, element):
+    row_data = []
+    for field in row.find_all(element):
+        data.append(field.text.strip())
+    return row_data
