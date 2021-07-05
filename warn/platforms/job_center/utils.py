b3f4a0871106f147698945872096e3e12cc272aa
Serdar Tumgoren <zstumgoren@gmail.com>
2021-07-05 08:33:40 -0700

    Finish output csv and dedupe

--- warn/platforms/job_center/utils.py
+++ warn/platforms/job_center/utils.py
@@ -1,9 +1,13 @@
+import logging
+import re
+from collections import OrderedDict
 from datetime import datetime as dt
 from pathlib import Path
 
 from .site import Site as  JobCenterSite
 from warn.utils import write_dict_rows_to_csv
 
+logger = logging.getLogger(__name__)
 
 
 def scrape_state(state_postal, search_url, output_csv, stop_year, cache_dir, use_cache=True):
@@ -15,6 +19,7 @@ def scrape_state(state_postal, search_url, output_csv, stop_year, cache_dir, use
       - Scrapes one year at a time, in reverse chronological order
       - Always does a fresh scrape for current and prior year
       - Uses cached files for years before current & prior
+      - Deduplicates search results
 
     Args:
 
@@ -30,6 +35,7 @@ def scrape_state(state_postal, search_url, output_csv, stop_year, cache_dir, use
         Full path to exported csv (e.g. ~/.warn-scraper/exports/ks.csv)
 
     """
+    raw_csv = f'{cache_dir}/{state_postal.lower()}_raw.csv'
     yearly_dates = _date_ranges_to_scrape(stop_year)
     # No caching should be used for current and prior year, so
     # we have to separate those from remaining years.
@@ -37,14 +43,19 @@ def scrape_state(state_postal, search_url, output_csv, stop_year, cache_dir, use
     # Set up scraper instance
     state_cache_dir = str(Path(cache_dir, state_postal.lower()))
     site = JobCenterSite(state_postal.upper(), search_url, cache_dir=state_cache_dir)
+    # Date-based searches produce search result pages that appear to have certain
+    # records duplicated over paged results. We'll initially write all data to a raw
+    # file which we then deduplicate to produce the final output_csv.
+    logger.debug(f'Generating {raw_csv}')
     # Execute the scrape in two batches
     # 1. Current and prior year. Always scrape fresh (i.e. never use cached files)
     #    in case records have been updated.
-    _scrape_years(site, output_csv, no_cache_years, use_cache=False)
+    _scrape_years(site, raw_csv, no_cache_years, use_cache=False)
     # 2. Years before current & prior, going back to stop_year.
     #    We should generally use cached files for these older years,
     #    since data is less likely to be updated.
-    _scrape_years(site, output_csv, yearly_dates, use_cache=use_cache)
+    _scrape_years(site, raw_csv, yearly_dates, use_cache=use_cache)
+    _dedupe(raw_csv, output_csv)
     return output_csv
 
 def _scrape_years(site, output_csv, start_end_dates, use_cache=True):
@@ -59,9 +70,10 @@ def _scrape_years(site, output_csv, start_end_dates, use_cache=True):
             'use_cache': use_cache,
         }
         pages_dict, data = site.scrape(**kwargs)
-        # Returned data includes search result page columns and
-        # data from a nested dict, e.g. address and # of layoffs!
-        headers = [k for k in data[0].keys() if k != 'detail']
+        headers = [
+            'employer', 'notice_date', 'number_of_employees_affected', 'warn_type',
+            'city', 'zip','lwib_area', 'address', 'record_number', 'detail_page_url',
+        ]
         #TODO: extend headers with fields from nested 'detail' record dict
         rows = [_prepare_row(row) for row in data]
         # Use write mode on current year, append mode for all others
@@ -69,7 +81,14 @@ def _scrape_years(site, output_csv, start_end_dates, use_cache=True):
         write_dict_rows_to_csv(output_csv, headers, rows, mode=write_mode)
 
 def _prepare_row(row):
-    row.pop('detail')
+    "Flatten the nested dict for downstream export to CSV"
+    # Returned data includes fields from search result page and
+    # data from detail page record for each layoff notice
+    # the latter contains two key fields (address and number affected)
+    detail = row.pop('detail')
+    row['number_of_employees_affected'] = detail['number_of_employees_affected']
+    row['address'] = re.sub(r'\n+', '; ', detail['address'].strip())
+    row['record_number'] = detail['record_number']
     return row
 
 def _date_ranges_to_scrape(stop_year):
@@ -86,3 +105,22 @@ def _date_ranges_to_scrape(stop_year):
             (start.format(year), end.format(year))
         )
     return yearly_dates
+
+def _dedupe(raw_csv, output_csv):
+    # Create an ordered dict to discard dupes while preserving row order
+    data = OrderedDict()
+    raw_count = 0
+    with open(raw_csv, 'r',newline='') as src:
+        for row in src:
+            raw_count += 1
+            data[row] = row
+    # Write the deduped rows to final output_csv
+    final_count = 0
+    with open(output_csv, 'w', newline='') as out:
+        for row in data.keys():
+            final_count += 1
+            out.write(row)
+    num_removed = raw_count - final_count
+    if num_removed > 0:
+        logger.debug(f'Removed {num_removed} duplicate records from {raw_csv}')
+    return output_csv
