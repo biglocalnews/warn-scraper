d3fd4198dcd47a618dc41570f29649d4428bce44
Charlotte Li <li.zihao@columbia.edu>
2021-07-09 15:23:43 -0400

    fixed buggy data, closes #129

--- warn/scrapers/md.py
+++ warn/scrapers/md.py
@@ -13,14 +13,12 @@ logger = logging.getLogger(__name__)
 
 def scrape(output_dir, cache_dir=None):
     output_csv = f'{output_dir}/md.csv'
-    # max_entries = 378 # manually inserted
-    years = range(2019, 2009, -1)
     url = 'http://www.dllr.state.md.us/employment/warn.shtml'
-    page = requests.get(url)
-    logger.debug(f"Page status is {page.status_code} for {url}")
-    soup = BeautifulSoup(page.text, 'html.parser')
-    table = soup.find_all('table') # output is list-type
+    html = scrape_page(url)
+
     # find header
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find_all('table') # output is list-type
     first_row = table[0].find_all('tr')[0]
     headers = first_row.find_all('td')
     output_header = []
@@ -33,14 +31,15 @@ def scrape(output_dir, cache_dir=None):
         writer = csv.writer(csvfile)
         writer.writerow(output_header)
 
+    # get the list of links and scrape the current page
     li_list = soup.find_all("a",{"class":"sub"})
 
     url = li_list[0]['href']
     url = f'http://www.dllr.state.md.us/employment/{url}'
-    html = requests.get(url)
-    logger.debug(f"Page status is {html.status_code} for {url}")
-    write_body(html.text,output_csv)
+    html = scrape_page(url)
+    write_body(html,output_csv)
 
+    # cache the old pages 
     cache = Cache(cache_dir) # ~/.warn-scraper/cache
     scrape_old(li_list[1:],cache, output_csv)
     return output_csv
@@ -51,17 +50,19 @@ def write_body(html,output_csv):
     output_rows = []
     for table_row in table[0].find_all('tr'):
         columns = table_row.find_all('td')
+        #print(len(columns)) --> one row in 2012 has 176 'td' for some reason
         output_row = []
         for column in columns:
             clean_txt = re.sub(r'\n', ' ', column.text)
             clean_txt = re.sub(r'\s+', ' ', clean_txt)
             output_row.append(clean_txt.strip())
         if output_row[0] != "Notice Date":
-            output_rows.append(output_row)
+            output_rows.append(output_row[:8]) # hard-coding a cutoff length to deal with the buggy long row
     write_rows_to_csv(output_rows,output_csv,mode='a')
 
 def scrape_page(url):
     response= requests.get(url)
+    logger.debug(f"Page status is {response.status_code} for {url}")
     response.encoding = 'utf-8'
     return response.text
 
