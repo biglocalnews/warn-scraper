d59e37ce51c6cf71704ce6d35204ac932ad0e7ec
Charlotte Li <li.zihao@columbia.edu>
2021-07-09 14:52:13 -0400

    added caching feature

--- warn/scrapers/md.py
+++ warn/scrapers/md.py
@@ -5,6 +5,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
+from warn.cache import Cache
 from warn.utils import write_rows_to_csv
 
 logger = logging.getLogger(__name__)
@@ -34,19 +35,21 @@ def scrape(output_dir, cache_dir=None):
 
     li_list = soup.find_all("a",{"class":"sub"})
 
-    for a in li_list:
-        url = a['href']
-        full_url = f'http://www.dllr.state.md.us/employment/{url}'
-        page = requests.get(full_url)
-        logger.debug(f"Page status is {page.status_code} for {full_url}")
-        soup = BeautifulSoup(page.text, 'html.parser')
-        table = soup.find_all('table') # output is list-type
-        write_body(table[0],output_csv)
+    url = li_list[0]['href']
+    url = f'http://www.dllr.state.md.us/employment/{url}'
+    html = requests.get(url)
+    logger.debug(f"Page status is {html.status_code} for {url}")
+    write_body(html.text,output_csv)
+
+    cache = Cache(cache_dir) # ~/.warn-scraper/cache
+    scrape_old(li_list[1:],cache, output_csv)
     return output_csv
 
-def write_body(table,output_csv):
+def write_body(html,output_csv):
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find_all('table') # output is list-type
     output_rows = []
-    for table_row in table.find_all('tr'):
+    for table_row in table[0].find_all('tr'):
         columns = table_row.find_all('td')
         output_row = []
         for column in columns:
@@ -56,3 +59,24 @@ def write_body(table,output_csv):
         if output_row[0] != "Notice Date":
             output_rows.append(output_row)
     write_rows_to_csv(output_rows,output_csv,mode='a')
+
+def scrape_page(url):
+    response= requests.get(url)
+    response.encoding = 'utf-8'
+    return response.text
+
+def scrape_old(url_list,cache, output_csv):
+    for a in url_list:
+        url = a['href']
+        # Add the state postal as cache key prefix
+        cache_key = f'md/{url}'
+        try:
+            html = cache.read(cache_key)
+            logger.debug(f'Page fetched from cache: {cache_key}')
+        except FileNotFoundError:
+            # If file not found in cache, scrape the page and save to cache
+            url = f'http://www.dllr.state.md.us/employment/{url}'
+            html = scrape_page(url)
+            cache.write(cache_key, html)
+            logger.debug(f"Scraped and cached {url}")
+        write_body(html,output_csv)
