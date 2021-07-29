cc6d0ce6ee0a12386b5a3dd5b1daaa289a85fc4c
Cody Hergenroeder <chergenroeder135@gmail.com>
2021-07-29 11:59:35 -0700

    fix excel pandas error: specify use openpyxl. closes #199, closes #200

--- warn/scrapers/ca.py
+++ warn/scrapers/ca.py
@@ -3,22 +3,22 @@ import logging
 import pandas as pd
 
 
-logger  = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def scrape(output_dir, cache_dir=None):
     url = 'https://www.edd.ca.gov/jobs_and_training/warn/WARN_Report.xlsx'
-    df = pd.read_excel(url)
+    df = pd.read_excel(url, engine="openpyxl")
     cali_data_path = f'{output_dir}/california_warn_raw.csv'
     df.to_csv(cali_data_path)
     ca_data = pd.read_csv(cali_data_path)
-    ca_data = ca_data.iloc[:,1:]
+    ca_data = ca_data.iloc[:, 1:]
     ca_data.dropna(axis=0, how='all', inplace=True)
     ca_data = ca_data[1:]
     headers = ca_data.iloc[0]
     ca_data.columns = headers
     ca_data = ca_data[1:]
-    ca_data.columns = ca_data.columns.str.replace('\\n',' ')
+    ca_data.columns = ca_data.columns.str.replace('\\n', ' ')
     try:
         ca_data = ca_data[['Notice Date', 'Effective Date', 'Received Date', 'Company', 'City', 'County', 'No. Of Employees ', 'Layoff/Closure']]
     except:
@@ -28,7 +28,7 @@ def scrape(output_dir, cache_dir=None):
     try:
         ca_data.rename(columns={"Layoff/Closure Type": "Layoff/Closure"}, inplace=True)
     except:
-         logger.error('There is no Layoff/Closure Type column to rename')
+        logger.error('There is no Layoff/Closure Type column to rename')
     ca_data['Notice Date'] = pd.to_datetime(ca_data['Notice Date'])
     ca_data['Effective Date'] = pd.to_datetime(ca_data['Effective Date'])
     ca_data['Received Date'] = pd.to_datetime(ca_data['Received Date'])
@@ -42,4 +42,3 @@ def scrape(output_dir, cache_dir=None):
     output_file = f'{output_dir}/california_warn_processed.csv'
     all_ca_data.to_csv(output_file, index=False)
     return output_file
-
--- warn/scrapers/mt.py
+++ warn/scrapers/mt.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def scrape(output_dir, cache_dir=None):
     url = 'https://wsd.dli.mt.gov/_docs/wioa/warn.xlsx'
-    df = pd.read_excel(url)
+    df = pd.read_excel(url, engine="openpyxl")
     output_file = f'{output_dir}/mt.csv'
     df.to_csv(output_file, index=False)
     return output_file
