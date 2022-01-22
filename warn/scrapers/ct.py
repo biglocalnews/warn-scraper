bea4ad49c007b20e2b885cb3b0ef48e56b70a148
palewire <b@palewi.re>
2022-01-22 03:33:49 -0800

    A little CT cleanup

--- warn/scrapers/ct.py
+++ warn/scrapers/ct.py
@@ -1,13 +1,17 @@
-import csv
 import logging
+from datetime import datetime
 from pathlib import Path
-import datetime
 
 from bs4 import BeautifulSoup
 
 from .. import utils
+from ..cache import Cache
 
-__authors__ = ["zstumgoren", "Dilcia19"]
+__authors__ = [
+    "zstumgoren",
+    "Dilcia19",
+    "stucka",
+]
 __tags__ = ["html"]
 
 logger = logging.getLogger(__name__)
@@ -26,11 +30,41 @@ def scrape(
 
     Returns: the Path where the file is written
     """
-    output_csv = data_dir / "ct.csv"
+    # Open the cache
+    cache = Cache(cache_dir)
 
-    # Hardcoded start year of 2015
-    years = list(reversed(range(2015, datetime.datetime.now().year + 1)))
+    # We start in 2015
+    current_year = datetime.now().year
 
+    # Get the full range of years
+    year_range = range(2015, current_year + 1)
+
+    output_rows = []
+    for year in year_range:
+        url = f"https://www.ctdol.state.ct.us/progsupt/bussrvce/warnreports/warn{year}.htm"
+        cache_key = f"ct/{year}.html"
+
+        if cache.exists(cache_key) and year < current_year:
+            html = cache.read(cache_key)
+        else:
+            r = utils.get_url(url)
+            html = r.text
+            cache.write(cache_key, html)
+
+        # Parse out the table
+        soup = BeautifulSoup(html, "html.parser")
+        if year == 2016:
+            table = soup.find_all("table", "style15")
+        else:
+            table = soup.find_all("table", "MsoNormalTable")
+
+        # Parse out the data
+        row_list = _scrape_table(table)
+
+        # Add data to the big list
+        output_rows.extend(row_list)
+
+    # Tack headers on the top
     header_row = [
         "warn_date",
         "affected_company",
@@ -42,55 +76,60 @@ def scrape(
         "union",
         "union_address",
     ]
-    output_rows = []
-    for year in years:
-        url = f"https://www.ctdol.state.ct.us/progsupt/bussrvce/warnreports/warn{year}.htm"
-        response = utils.get_url(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        if year == 2016:
-            table = soup.find_all("table", "style15")
-        else:
-            table = soup.find_all("table", "MsoNormalTable")
-        table_rows = table[0].find_all("tr")
-        # loop over table to process each row
-        for table_row in table_rows:
-            # skip 1st row because it is the header
-            if table_rows.index(table_row) == 0:
-                continue
-            table_cells = table_row.find_all("td")
-            # if a row has more than 9 cells it is handled separately
-            # the 2016 table has some cells with nested tags
-            if len(table_cells) > 9:
-                output_row = _problem_cells(table_cells)
-                output_rows.append(output_row)
-            # if a row has less than 9 it is skipped because it is incomplete
-            elif len(table_cells) < 9:
-                continue
-            else:
-                # loop over cells for each row
-                output_row = []
-                for table_cell in table_cells:
-                    cell = table_cell.text.strip()
-                    cell = " ".join(cell.split())
-                    output_row.append(cell)
-                # test to see if the row is blank
-                if not output_row:
-                    continue
-                output_rows.append(output_row)
-        logger.debug(
-            f"Scraping {len(output_rows)} total rows back through year {year}."
-        )
+    row_list = header_row + output_rows
 
-    # save to csv
-    with open(output_csv, "w") as out:
-        writer = csv.writer(out)
-        writer.writerow(header_row)
-        writer.writerows(output_rows)
-    return output_csv
+    # Set the export path
+    data_path = data_dir / "ct.csv"
+
+    # Write out to csv
+    utils.write_rows_to_csv(row_list, data_path)
+
+    # Return the path
+    return data_path
 
 
-# function to deal with problem rows in the 2016 table
+def _scrape_table(table) -> list:
+    """Scrape the provided table.
+
+    Returns: List of data rows.
+    """
+    row_list = []
+    # loop over table to process each row, skipping the header
+    for table_row in table[0].find_all("tr")[1:]:
+
+        # Get all the cells
+        table_cells = table_row.find_all("td")
+
+        # if a row has more than 9 cells it is handled separately
+        # the 2016 table has some cells with nested tags
+        if len(table_cells) > 9:
+            output_row = _problem_cells(table_cells)
+            row_list.append(output_row)
+        # if a row has less than 9 it is skipped because it is incomplete
+        elif len(table_cells) < 9:
+            continue
+
+        # for the rest, loop over cells for each row
+        output_row = []
+        for table_cell in table_cells:
+            cell = table_cell.text.strip()
+            cell = " ".join(cell.split())
+            row_list.append(cell)
+
+        # test to see if the row is blank
+        if not output_row:
+            continue
+
+        # Add row to the big list
+        row_list.append(output_row)
+
+    # Pass it back
+    logger.debug(f"{len(row_list)} rows parsed")
+    return row_list
+
+
 def _problem_cells(table_cells):
+    """Deal with problem rows in the 2016 table."""
     output_row = []
     for table_cell in table_cells:
         current_cell = table_cell.text.strip()
