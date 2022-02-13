11f086dba810c422f4a8a10d7768c3d1378c3a52
palewire <b@palewi.re>
2022-02-13 06:55:08 -0800

    Switched NJ to use an Excel sheet

--- warn/scrapers/nj.py
+++ warn/scrapers/nj.py
@@ -1,13 +1,12 @@
-import itertools
 import logging
 from pathlib import Path
 
-from bs4 import BeautifulSoup
+from openpyxl import load_workbook
 
 from .. import utils
 from ..cache import Cache
 
-__authors__ = ["zstumgoren", "Dilcia19"]
+__authors__ = ["zstumgoren", "Dilcia19", "palewire"]
 __tags__ = ["html"]
 
 logger = logging.getLogger(__name__)
@@ -29,83 +28,55 @@ def scrape(
     # Fire up the cache
     cache = Cache(cache_dir)
 
-    # Get the root URL
-    url = "http://lwd.state.nj.us/WorkForceDirectory/warn.jsp"
-    r = utils.get_url(url)
-    r.encoding = "utf-8"
-    html = r.text
+    # Download URL
+    url = "https://www.nj.gov/labor/assets/PDFs/WARN/WARN_Notice_Archive.xlsx"
+    wb_path = cache.download("nj/source.xlsx", url)
 
-    # Cache the result
-    cache.write("nj/source.html", html)
-
-    # Parse out the tables
-    soup = BeautifulSoup(html, "html.parser")
-    table_list = soup.find_all("table")
-
-    # Each row is its own table. Yes, I know.
+    # Read in the workbook
     output_rows = []
-    for table in table_list:
-        cell_list = [c.text.strip() for c in table.find_all("td")]
-        output_rows.append(cell_list)
+    wb = load_workbook(filename=wb_path)
+    for ws in wb.worksheets:
+        logger.debug(f"Parsing {ws}")
+        for i, row in enumerate(ws.rows):
 
-    # Get historical data from separate source
-    years = [2010, 2009, 2008, 2007, 2006, 2005, 2004]
-    months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-    ]
-    year_month_pairs = itertools.product(years, months)  # [(2010, 'Jan'), ...]
-    for year, month in year_month_pairs:
-        html_page_name = f"{month}{year}Warn.html"
-        # Add the state postal as cache key prefix
-        cache_key = f"nj/{html_page_name}"
-        # Get the HTML, trying the cache first
-        if cache.exists(cache_key):
-            html = cache.read(cache_key)
-        else:
-            # If file not found in cache, scrape the page and save to cache
-            url = f"https://www.nj.gov/labor/lwdhome/warn/{year}/{html_page_name}"
-            r = utils.get_url(url)
-            r.encoding = "utf-8"
-            html = r.text
-            cache.write(cache_key, html)
-
-        # Parse out the tables
-        soup = BeautifulSoup(html, "html5lib")
-        table_list = soup.find_all("table")
-        table = table_list[0]
-
-        # Loop through the rows
-        for i, row in enumerate(table.find_all("tr")):
-
-            # Skip the header row
+            # Skip header
             if i == 0:
                 continue
 
-            # Parse the data
-            cell_list = [c.text.strip() for c in row.find_all("td")]
+            # A list with only empty cells will throw an error
+            try:
+                next(v for v in row if v.value)
+            except StopIteration:
+                continue
 
-            # Add it to the pile
-            output_rows.append(cell_list)
+            # Parse out data
+            d = {
+                "Company": _parse_value(row[0]),
+                "City": _parse_value(row[1]),
+                "Month Posted": _parse_value(row[2]),
+                "Effective Date": _parse_value(row[3]),
+                "Workforce Affected": _parse_value(row[4]),
+            }
+
+            # Tack it on
+            output_rows.append(d)
 
     # Set the export path
     data_path = data_dir / "nj.csv"
 
     # Write out the file
-    utils.write_rows_to_csv(data_path, output_rows)
+    headers = output_rows[0].keys()
+    utils.write_dict_rows_to_csv(data_path, headers, output_rows)
 
     # Return the path to the file
     return data_path
+
+
+def _parse_value(cell):
+    v = cell.value
+    if isinstance(v, str):
+        return v.strip()
+    return v
 
 
 if __name__ == "__main__":
