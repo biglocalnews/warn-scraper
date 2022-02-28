697e2bcf27cc403b0041cf7013748bf99d6d4a91
palewire <b@palewi.re>
2022-02-28 13:35:11 -0800

    Some small code review tweaks to the TN scraper

--- warn/scrapers/tn.py
+++ warn/scrapers/tn.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 import pdfplumber
-import requests
 from bs4 import BeautifulSoup
 
 from .. import utils
 from ..cache import Cache
+
+__authors__ = ["anikasikka"]
+__tags__ = ["html", "pdf"]
 
 
 def scrape(
@@ -21,26 +23,21 @@ def scrape(
 
     Returns: the Path where the file is written
     """
-    # Grab the page
+    # Initialize the cache
+    cache = Cache(cache_dir)
+
+    # Grab the HTML page with the latest years of data
     page = utils.get_url(
         "https://www.tn.gov/workforce/general-resources/major-publications0/major-publications-redirect/reports.html"
     )
     html = page.text
-    pdf_url = "https://www.tn.gov/content/dam/tn/workforce/documents/majorpublications/reports/WarnReportByMonth.pdf"
-    pdf_file = download(pdf_url)
-
-    # Write the raw file to the cache
-    cache = Cache(cache_dir)
     cache.write("tn/source.html", html)
-    cache.write("tn/pdffile.pdf", pdf_file)
 
-    # Parse the source file and convert to a list of rows, with a header in the first row.
-    # It's up to you to fill in the blank here based on the structure of the source file.
-    # You could do that here with BeautifulSoup or whatever other technique.
-    soup = BeautifulSoup(html, "html.parser")
-    data_list = soup.find_all("p")
+    # Grab the PDF with the archived historial data
+    pdf_url = "https://www.tn.gov/content/dam/tn/workforce/documents/majorpublications/reports/WarnReportByMonth.pdf"
+    pdf_file = cache.download("tn/pdffile.pdf", pdf_url)
 
-    # getting headers from pdf
+    # Set the headers we'll use for both sources
     tn_headers = [
         "Notice Date",
         "Effective Date",
@@ -50,59 +47,92 @@ def scrape(
         "County",
         "No. Of Employees",
         "Layoff/Closure",
+        "Notice ID",
     ]
     cleaned_data = [tn_headers]
 
-    i = 0
-    for data in data_list:
-        if i != 0:
-            # splitting the data on its delimiter
-            items = str(data).split("|")
-            raw_data = []
-            # making sure that the last item in the list is the data value of interest; splitting based on last character of each text-html data sequence
-            for item in items:
-                components = (
-                    item.replace("</a>", "")
-                    .replace("</p>", "")
-                    .replace("<strong>", "")
-                    .replace("<br/>", "")
-                    .split(">")
-                )
-                raw_data.append(components[-1])
-            nice_data = []
-            # discarding bad data — only extracting data that has all the relevant information
-            if len(raw_data) == 6:
-                nice_data.append(raw_data[0])
-                nice_data.append(raw_data[4])
-                nice_data.append("")
-                nice_data.append(raw_data[1])
-                nice_data.append("")
-                nice_data.append(raw_data[2])
-                nice_data.append(raw_data[3])
-                nice_data.append(raw_data[5])
-                cleaned_data.append(nice_data)
-        i += 1
+    # Parse the latest HTML file and convert to a list of rows, with a header in the first row.
+    soup = BeautifulSoup(html, "html5lib")
 
+    # Grab all the list items on the page
+    data_list = soup.find_all("p")
+
+    # Loop through them all, skipping the first item, which is a header
+    for data in data_list[1:]:
+
+        # splitting the data on its delimiter
+        items = str(data).split("|")
+
+        # making sure that the last item in the list is the data value of interest
+        # splitting based on last character of each text-html data sequence
+        raw_data = []
+        for item in items:
+            value_html = item.split(":")[-1]
+            value_soup = BeautifulSoup(value_html, "html5lib")
+            string_list = list(value_soup.stripped_strings)
+            value = string_list[-1]
+            raw_data.append(value)
+
+        # If there aren't six entries it's junk
+        if len(raw_data) != 6:
+            continue
+
+        # Pluck out the values we want
+        nice_data = [
+            raw_data[0],  # Notice Date
+            raw_data[4],  # Effective Date
+            "",  # Received Date
+            raw_data[1],  # Company
+            "",  # City
+            raw_data[2],  # County
+            raw_data[3],  # Number of employees
+            "",  # Layoff/Closure
+            raw_data[5],  # Notice ID
+        ]
+
+        # Add them to the master list
+        cleaned_data.append(nice_data)
+
+    # The PDF header blacklist of rows to toss
+    pdf_header_blacklist = [
+        "Notice Date",
+        "Total",
+    ]
+
+    # Open the PDF
     with pdfplumber.open(pdf_file) as pdf:
-        i = 0
-        for my_page in pdf.pages:
-            # all even pages have data, odd pages don't have the data
-            if i % 2 == 0:
-                all_extracted_tables = my_page.extract_tables()
-                for table in all_extracted_tables:
-                    for rows in table:
-                        # to remove redundant headers
-                        if (
-                            rows[0] == "Notice Date"
-                            or rows[0] == ""
-                            or rows[0] == "Summary by Month"
-                        ):
-                            continue
-                        cleaned_data.append(rows)
-            i += 1
+
+        # Loop through all the pages
+        for i, my_page in enumerate(pdf.pages):
+
+            # Sll even pages have data, odd pages don't have the data
+            if i % 2 != 0:
+                continue
+
+            # Pull out the table and loop through the rows
+            table = my_page.extract_table()
+
+            # Cut empty rows
+            row_list = [r for r in table if any(r)]
+
+            # If this is a summary table, skip it
+            if row_list[0][0].lower().strip() == "summary by month":
+                continue
+
+            # Loop through all the rows ...
+            for row in row_list:
+
+                # Skip remove redundant headers
+                if row[0] in pdf_header_blacklist:
+                    continue
+
+                # Toss in an empty Notice ID since it isn't in the PDF
+                row.append("")
+
+                # Add the data to our output
+                cleaned_data.append(row)
 
     # Set the path to the final CSV
-    # We should always use the lower-case state postal code, like nj.csv
     output_csv = data_dir / "tn.csv"
 
     # Write out the rows to the export directory
@@ -110,16 +140,6 @@ def scrape(
 
     # Return the path to the final CSV
     return output_csv
-
-
-def download(url):
-    """Download the pdf url."""
-    file = url.split("/")[-1]
-    with requests.get(url) as r:
-        with open(file, "wb") as f:
-            f.write(r.content)
-
-    return file
 
 
 if __name__ == "__main__":
