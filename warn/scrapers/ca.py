344b3802f6637200f5459d4cd3afe92dd1c6eca0
palewire <b@palewi.re>
2022-03-18 09:53:38 -0700

    Another CA fix

--- warn/scrapers/ca.py
+++ warn/scrapers/ca.py
@@ -123,7 +123,7 @@ def _extract_excel_data(wb_path):
             "company": row[5].value.strip(),
             "layoff_or_closure": row[8].value.strip(),
             "num_employees": row[9].value,
-            "address": row[11].value.strip(),
+            "address": row[12].value.strip(),
             "source_file": str(wb_path).split("/")[-1],
         }
         payload.append(data)
