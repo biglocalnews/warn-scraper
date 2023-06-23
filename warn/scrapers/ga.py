3153a5a6048d0853724278dbf9e77e7f3e3197d4
Ash1R <ashir.rao26@gmail.com>
2023-06-22 17:55:22 -0700

    GA scraper using nonce

--- warn/scrapers/ga.py
+++ warn/scrapers/ga.py
@@ -1,23 +1,17 @@
-import logging
 import re
-import time
-import typing
-from datetime import datetime
 from pathlib import Path
 
+import requests
 from bs4 import BeautifulSoup
 
 from .. import utils
-from ..cache import Cache
 
-__authors__ = ["chriszs"]
+__authors__ = ["chriszs, esagara, Ash1R"]
 __tags__ = ["html"]
 __source__ = {
     "name": "Georgia Department of Labor",
     "url": "https://www.dol.state.ga.us/public/es/warn/searchwarns/list",
 }
-
-logger = logging.getLogger(__name__)
 
 
 def scrape(
@@ -33,105 +27,101 @@ def scrape(
 
     Returns: the Path where the file is written
     """
-    state_code = "ga"
-    cache = Cache(cache_dir)
+    base_url = "https://www.tcsg.edu/warn-public-view/"
 
-    # The basic configuration for the scrape
-    base_url = "http://www.dol.state.ga.us/public/es/warn/searchwarns/list"
+    api_url = "https://www.tcsg.edu/wp-admin/admin-ajax.php"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    }
 
-    area = 9  # statewide
+    response = utils.get_url(base_url, headers=headers)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text)
 
-    current_year = datetime.now().year
-    first_year = 1989  # first available year
-
-    years = list(range(first_year, current_year + 1))
-    years.reverse()
-
-    # Loop through the years and scrape them one by one
-    output_rows: typing.List = []
-    for i, year in enumerate(years):
-        # Concoct the URL
-        url = f"{base_url}?geoArea={area}&year={year}&step=search"
-        cache_key = f"{state_code}/{year}.html"
-
-        # Read from cache if available and not this year or the year before
-        if cache.exists(cache_key) and year < current_year - 1:
-            html = cache.read(cache_key)
-        else:
-            # Otherwise, go request it
-            page = utils.get_url(url, verify=False)
-            html = page.text
-            cache.write(cache_key, html)
-
-        # Scrape out the table
-        new_rows = _parse_table(
-            html,
-            "emplrList",
-            include_headers=len(output_rows) == 0 or i == 0,  # After the first loop, we can skip the headers
+    script = str(
+        soup.find(
+            "script", text=lambda text: text and "window.gvDTglobals.push" in text
         )
+    )
 
-        # Concatenate the rows
-        output_rows.extend(new_rows)
+    match = re.search(r'"nonce":"([^"]+)"', script)
+    if match:
+        nonce = match.group(1)
 
-        # Sleep a bit
-        time.sleep(2)
-
-    # Write out the results
-    data_path = data_dir / f"{state_code}.csv"
-    utils.write_rows_to_csv(data_path, output_rows)
-
-    # Return the path to the CSV
-    return data_path
-
-
-def _parse_table(html, id, include_headers=True) -> typing.List:
-    """
-    Parse HTML table with given ID.
-
-    Keyword arguments:
-    html -- the HTML to parse
-    id -- the ID of the table to parse
-    include_headers -- whether to include the headers in the output (default True)
-
-    Returns: a list of rows
-    """
-    # Parse out data table
-    soup = BeautifulSoup(html, "html5lib")
-    table_list = soup.find_all(id=id)  # output is list-type
-
-    # We expect the first table to be there with our data
-    try:
-        assert len(table_list) > 0
-    except AssertionError:
-        logger.debug("No tables found")
-        return []
-    table = table_list[0]
-
-    output_rows = []
-    column_tags = ["td"]
-
-    if include_headers:
-        column_tags.append("th")
-
-    # Loop through the table and grab the data
-    for table_row in table.find_all("tr"):
-        columns = table_row.find_all(column_tags)
-        output_row = []
-
-        for column in columns:
-            # Collapse newlines
-            partial = re.sub(r"\n", " ", column.text)
-            # Standardize whitespace
-            clean_text = re.sub(r"\s+", " ", partial).strip()
-            output_row.append(clean_text)
-
-        # Skip any empty rows
-        if len(output_row) == 0 or output_row == [""]:
-            continue
-
-        output_rows.append(output_row)
-
-    return output_rows
+    payload = {
+        "draw": 1,
+        "columns": [
+            {
+                "data": 0,
+                "name": "gv_96",
+                "searchable": True,
+                "orderable": True,
+                "search": {
+                    "value": None,
+                    "regex": False,
+                },
+            },
+            {
+                "data": 1,
+                "name": "gv_4",
+                "searchable": True,
+                "orderable": True,
+                "search": {
+                    "value": None,
+                    "regex": False,
+                },
+            },
+            {
+                "data": 2,
+                "name": "gv_date_created",
+                "searchable": True,
+                "orderable": True,
+                "search": {
+                    "value": None,
+                    "regex": False,
+                },
+            },
+            {
+                "data": 3,
+                "name": "gv_97",
+                "searchable": True,
+                "orderable": True,
+                "search": {
+                    "value": None,
+                    "regex": False,
+                },
+            },
+        ],
+        "order": [{"column": 0, "dir": "asc"}],
+        "start": 0,
+        "length": 25,
+        "search": {
+            "value": None,
+            "regex": False,
+        },
+        "action": "gv_datatables_data",
+        "view_id": 77460,
+        "post_id": 77462,
+        "nonce": nonce,
+        "getData": [],
+        "hideUntilSearched": 0,
+        "setUrlOnSearch": True,
+        "shortcode_atts": {"id": 77460, "class": None, "detail": None},
+    }
+    response = requests.post(api_url, data=payload, headers=headers)
+    pdf_headers = ["company", "date", "# affected", "url"]
+    final = [pdf_headers]
+    data = response.json()["data"]
+    for listing in data:
+        row = []
+        row.append(listing[1])
+        row.append(listing[2])
+        row.append(listing[3])
+        row.append(BeautifulSoup(listing[0]).text)
+        final.append(row)
+    output_csv = data_dir / "ga.csv"
+    utils.write_rows_to_csv(output_csv, final)
+    return output_csv
 
 
 if __name__ == "__main__":
