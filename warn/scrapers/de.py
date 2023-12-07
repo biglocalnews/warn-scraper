06616238b686759508c688893f68445ddaa73e6e
Mike Stucka <stucka@whitedoggies.com>
2023-12-07 15:14:24 -0500

    Patch DE around bad SSL cert, fix a few lil things

--- warn/scrapers/de.py
+++ warn/scrapers/de.py
@@ -4,7 +4,7 @@ from warn.platforms.job_center.utils import scrape_state
 
 from .. import utils
 
-__authors__ = ["zstumgoren", "Dilcia19"]
+__authors__ = ["zstumgoren", "Dilcia19", "stucka"]
 __tags__ = ["jobcenter"]
 __source__ = {
     "name": "Delaware Department of Labor",
@@ -18,7 +18,7 @@ def scrape(
     use_cache: bool = True,
 ) -> Path:
     """
-    Scrape data from Connecticut.
+    Scrape data from Delaware.
 
     Arguments:
     output_dir -- the Path were the result will be saved
@@ -38,7 +38,13 @@ def scrape(
 
     # Use cache for years before current and prior year
     scrape_state(
-        "DE", search_url, output_csv, stop_year, cache_dir, use_cache=use_cache
+        "DE",
+        search_url,
+        output_csv,
+        stop_year,
+        cache_dir,
+        use_cache=use_cache,
+        verify=False,
     )
 
     # Return the resulting CSV file path
