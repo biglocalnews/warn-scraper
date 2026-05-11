3f75425ba4c5701286248908d3c41011a156dc3d
Mike Stucka <stucka@whitedoggies.com>
2026-05-11 12:34:20 -0400

    Clean

--- setup.py
+++ setup.py
@@ -22,37 +22,6 @@ def read(file_name):
         return f.read()
 
 
-def version_scheme(version):
-    """
-    Version scheme hack for setuptools_scm.
-
-    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
-
-    If that issue is resolved, this method can be removed.
-    """
-    import time
-
-    from setuptools_scm.version import guess_next_version
-
-    if version.exact:
-        return version.format_with("{tag}")
-    else:
-        _super_value = version.format_next_version(guess_next_version)
-        now = int(time.time())
-        return _super_value + str(now)
-
-
-def local_version(version):
-    """
-    Local version scheme hack for setuptools_scm.
-
-    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
-
-    If that issue is resolved, this method can be removed.
-    """
-    return ""
-
-
 class TallyCommand(distutils.cmd.Command):
     """Tally scrapers and update sources dashboard in the docs."""
 
